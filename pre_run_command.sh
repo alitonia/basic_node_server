04b3961c5fbadd65ea34f4e6d4ca959b016426fb
Zen-net <allyofjustice1@gmail.com>
2021-04-02 04:03:56 +0700

    add image for product

--- pre_run_command.sh
+++ pre_run_command.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+chmod u+x scripts/extract_banner/extract.sh
+./scripts/extract_banner/extract.sh
+
 chmod u+x scripts/pull_images/pull.sh
 ./scripts/pull_images/pull.sh
--- scripts/pull_images/pull.sh
+++ scripts/pull_images/pull.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+IMAGE_LIMIT=1500 # might be smaller due to duplication
 DATA_URL=https://s3.eu-central-1.amazonaws.com/fashion-gan/images.zip
 ZIP_FILE=scripts/pull_images/images.zip
 IMAGE_DESTINATION=client/public/images/product/
@@ -16,10 +17,11 @@ rm -rf $IMAGE_DESTINATION
 mkdir -p $IMAGE_DESTINATION
 
 echo "Unzipping files..."
-unzip -j $ZIP_FILE images/test_images/dresses/* -d $IMAGE_DESTINATION
+unzip -Z1 scripts/pull_images/images.zip | grep -E "(clustered|test)" | head -$IMAGE_LIMIT | xargs unzip -jn $ZIP_FILE -d $IMAGE_DESTINATION
 
 echo "Renaming image sequentially..."
 ls $IMAGE_DESTINATION | cat -n | while read n f; do mv $IMAGE_DESTINATION"$f" $IMAGE_DESTINATION"$n.jpg"; done #
 ##rm *.zip
 #
 echo "done"
+echo $count
