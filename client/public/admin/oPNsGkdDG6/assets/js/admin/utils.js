09133e5a0972d4b35bd1993786bec64c30e2847e
Zen-net <allyofjustice1@gmail.com>
2021-06-16 12:01:58 +0700

    add displayer

--- client/public/admin/oPNsGkdDG6/assets/js/admin/utils.js
+++ client/public/admin/oPNsGkdDG6/assets/js/admin/utils.js
@@ -17,3 +17,21 @@ const url_mutation = (newParams = {}) => {
 }
 
 const changePage = (link) => window.location.href = link
+
+function removeParam(key, sourceURL) {
+    let rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (let i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
+}
--- pg_database/admin/getAllReceipts.js
+++ pg_database/admin/getAllReceipts.js
@@ -1,11 +1,11 @@
 const pool = require('../connect_database.js');
 
-const get_receipts = (sortby, order, offset, limit) => {
+const get_receipts = (field, order, offset, limit, searchby) => {
     return (
         `SELECT *
          FROM receipts
-         where status != 'created'
-         ORDER BY ${sortby} ${order}
+         where status != 'created' ${searchby ? `and CAST(${field} AS VARCHAR) ILIKE '%%${searchby}%'` : ''}
+         ORDER BY ${field} ${order}
          OFFSET ${offset} LIMIT ${limit}
         ;`
     )
@@ -16,13 +16,13 @@ const allowedOrder = ['desc', 'asc']
 
 
 module.exports.getAllReceipts = (req, res) => {
-    const {sortby = 'status', order = 'des', offset = 0, limit = 30} = req.query || {}
+    const {sortby = 'status', order = 'des', offset = 0, limit = 30, searchby = ''} = req.query || {}
 
     if (!allowedOrder.includes(order) || !allowedSortby.includes(sortby) || limit > 50) {
         return res.send({error: 'Something went wrong'})
     }
 
-    pool.query(get_receipts(sortby, order, offset, limit),
+    pool.query(get_receipts(sortby, order, offset, limit, searchby),
         (err, response) => {
             if (err || !response) {
                 res.status = 501
